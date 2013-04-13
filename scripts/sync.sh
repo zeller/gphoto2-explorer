90928c6f717d7a8a49d04453e15572dffcebf486
Michael Zeller <me@michaelzeller.com>
2013-04-13 20:27:47 +0000

    Create directories when necessary

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,11 +1,23 @@
+if [ ! -d ../thumbs ]; then
+    mkdir ../thumbs
+fi
+
 pushd ../thumbs
 # Download all thumbnails from the DSLR (fast)
 gphoto2 --get-all-thumbnails --new
 popd
 
+if [ ! -d ../photos ]; then
+    mkdir ../photos
+fi
+
 pushd ../photos
 # Download all photos from the DSLR (slow)
 gphoto2 --get-all-files --new
+
+if [ ! -d ../db ]; then
+    mkdir ../db
+fi
 
 # Extract EXIF datetime to a database
 python utils/extract-EXIF-data.py
